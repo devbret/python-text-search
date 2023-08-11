1caf45dff8ace864a56e4719fae61c42f21f5f46
Bret Bernhoft <73839896+devbret@users.noreply.github.com>
2023-08-10 19:35:36 -0700

    Added basic files.

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import re
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_string = request.json.get('searchString', '')
+    
+    # You can specify your text file's path here
+    file_path = 'file.txt'
+
+    result = []
+    with open(file_path, 'r') as file:
+        for line_number, line in enumerate(file, start=1):
+            if search_string in line:
+                result.append({
+                    'line_number': line_number,
+                    'line_content': line.strip()
+                })
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
